684acbb4cc0e7e1ca6c3081d4b89bfa41dc71c99
imauice <102501547+imauice@users.noreply.github.com>
2023-02-17 04:15:35 +0700

    Create Component.vue

---
+++ Component.vue
@@ -0,0 +1,10 @@
+export default {
+  computed: {
+    order() {
+      return this.$store.getters.getOrderById(this.orderId);
+    }
+  },
+  created() {
+    this.$store.dispatch('fetchOrders');
+  }
+};
